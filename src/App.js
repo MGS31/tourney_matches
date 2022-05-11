fbb3e6a11fb0329745a2eef3059d0e83800c3c4e
Matt <mgstankov30@gmail.com>
2022-05-11 11:27:24 -0400

    complete code for tourney matches

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import PlayerList from './components/PlayerList';
+import MatchList from './components/MatchList';
+import matchData from './data/matchData';
+import playerData from './data/playerData';
+import { preparePlayerData, addWinsToPlayers } from './helpers/playerHelper'
 
 function App() {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+
   return (
     <div className="App">
-     
+      <h1>
+      Tourney Matches <span>Where Coding and Tournaments found their Match!</span>
+      </h1>  
+      <PlayerList playerData={parsedPlayerData}/>
+      <MatchList matchData={matchData}/>
     </div>
   );
 }
